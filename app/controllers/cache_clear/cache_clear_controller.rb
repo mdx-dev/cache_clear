cf16a7f2c403416d6cc92853d301ac148c0d61b4
amcaplan <ariel.caplan@vitals.com>
2015-07-15 19:12:25 +0300

    Write a simple test and fix the controller action
    It was erroring out because there was no response specified.

--- app/controllers/cache_clear/cache_clear_controller.rb
+++ app/controllers/cache_clear/cache_clear_controller.rb
@@ -2,6 +2,7 @@ module CacheClear
   class CacheClearController < ApplicationController
     def clear_cache
       Rails.cache.clear
+      head :ok
     end
   end
 end
---
+++ spec/request/cache_clear_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'clearing the cache', type: :request, cache: true do
+  context 'with data in the cache' do
+    before do
+      Rails.cache.write('foo', 'bar')
+      expect(Rails.cache.read('foo')).to eq('bar')
+    end
+
+    it 'clears the cache' do
+      delete '/cache_clear/foo'
+      expect(Rails.cache.read('foo')).not_to eq('bar')
+    end
+  end
+end
